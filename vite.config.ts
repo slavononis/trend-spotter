97e37af3c7e50eada0151e86d7b6853167e24ffe
Slava Onischenko <slavon.onischenko@gmail.com>
2025-11-01 11:50:00 +0200

    feat(ts-1): setup shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path"
 
 export default defineConfig({
+  server: { port: 5173, host: '0.0.0.0' },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+   resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
+
